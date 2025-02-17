d5ba5ee1fc230d89beef43fdec353f18ffd4cb77
Dheerajp23 <59532632+Dheerajp23@users.noreply.github.com>
2025-02-17 20:54:24 +0530

    Post

--- script.js
+++ script.js
@@ -15,100 +15,129 @@ const workbtn = document.querySelectorAll('.work-btn');
 const navbtn = document.querySelector('.nav-btn');
 const show = document.querySelector('.show');
 
-const links= document.querySelectorAll('.links')
+const links = document.querySelectorAll('.links')
 
 
-change.addEventListener('click', function(){
-  
-   if(mode.classList.contains('fa-moon')){
-   
-    mode.classList.remove('fa-moon')
-    mode.classList.add('fa-sun')
-   }
-   else if(mode.classList.contains('fa-sun')){
-   
-   mode.classList.remove('fa-sun')
-   mode.classList.add('fa-moon')
-}
+change.addEventListener('click', function () {
+
+    if (mode.classList.contains('fa-moon')) {
+
+        mode.classList.remove('fa-moon')
+        mode.classList.add('fa-sun')
+    }
+    else if (mode.classList.contains('fa-sun')) {
+
+        mode.classList.remove('fa-sun')
+        mode.classList.add('fa-moon')
+    }
 })
 
 
-btnAll.addEventListener('click', function(){
+btnAll.addEventListener('click', function () {
     workbtn.forEach(btn => {
         btn.classList.remove('active-btn');
     })
     btnAll.classList.add('active-btn');
     workItem.forEach(item => {
-        if(item.classList.contains('all')){
-            item.style.display = 'block';}
-        else{
+        if (item.classList.contains('all')) {
+            item.style.display = 'block';
+        }
+        else {
             item.style.display = 'none';
         }
     })
 })
 
-btnreact.addEventListener('click', function(){
+btnreact.addEventListener('click', function () {
     workbtn.forEach(btn => {
         btn.classList.remove('active-btn');
     })
     btnreact.classList.add('active-btn');
     workItem.forEach(item => {
-        if(item.classList.contains('react')){
-            item.style.display = 'block';}
-        else{
+        if (item.classList.contains('react')) {
+            item.style.display = 'block';
+        }
+        else {
             item.style.display = 'none';
         }
     })
 })
-btnHtml.addEventListener('click', function(){
+btnHtml.addEventListener('click', function () {
     workbtn.forEach(btn => {
         btn.classList.remove('active-btn');
     })
     btnHtml.classList.add('active-btn');
     workItem.forEach(item => {
-        if(item.classList.contains('html')){
-            item.style.display = 'block';}
-        else{
+        if (item.classList.contains('html')) {
+            item.style.display = 'block';
+        }
+        else {
             item.style.display = 'none';
         }
     })
 })
-btnjs.addEventListener('click', function(){
+btnjs.addEventListener('click', function () {
     workbtn.forEach(btn => {
         btn.classList.remove('active-btn');
     })
     btnjs.classList.add('active-btn');
-   workItem.forEach(item => {
-    if(item.classList.contains('javascript')){
-        item.style.display = 'block';
-   }
-    else{
-         item.style.display = 'none';
-    }
+    workItem.forEach(item => {
+        if (item.classList.contains('javascript')) {
+            item.style.display = 'block';
+        }
+        else {
+            item.style.display = 'none';
+        }
     })
 })
 
 
-navbtn.addEventListener('click', function(){
-   if(show.style.visibility === 'hidden' || show.style.visibility === ''){  
-       show.style.visibility = 'visible';
-       show.style.opacity = '1';
-       show.style.height = 'fit-content';
-       navbtn.style.color = 'red'
+navbtn.addEventListener('click', function () {
+    if (show.style.visibility === 'hidden' || show.style.visibility === '') {
+        show.style.visibility = 'visible';
+        show.style.opacity = '1';
+        show.style.height = 'fit-content';
+        navbtn.style.color = 'red'
 
-    }else{  
+    } else {
         show.style.visibility = 'hidden';
-       show.style.opacity = '0';
-       show.style.transition = 'all 0.5s ease-in-out'; 
-       navbtn.style.color = 'white';
+        show.style.opacity = '0';
+        show.style.transition = 'all 0.5s ease-in-out';
+        navbtn.style.color = 'white';
     }
 })
 
 links.forEach(function (link) {
-    link.addEventListener('click',function(){
+    link.addEventListener('click', function () {
         show.style.visibility = 'hidden';
         show.style.opacity = '0';
-        show.style.transition = 'all 0.5s ease-in-out'; 
+        show.style.transition = 'all 0.5s ease-in-out';
         navbtn.style.color = 'white';
-    })   
+    })
+})
+
+
+document.querySelector('#contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = this.Username.value;
+    const email= this.email.value;
+    const number = this.number.value;
+    const message = this.message.value;
+    console.log(name);
+    console.log(email);
+    console.log(number);
+    console.log(message);
+  
+    Toastify({
+        text: "Thank you for reaching out to me, I will get back to you as soon as possible",
+        duration: 5000,
+        close: true,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+            background: "linear-gradient(to right,rgb(98, 0, 255),rgb(0, 4, 255))",
+        },
+    }).showToast();
+    document.querySelector('#contact-form').reset();
 })
